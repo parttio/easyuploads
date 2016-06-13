c756a3016426f814f16f9dcb7e994da4a8b3bc44
Matti Tahvonen <matti@vaadin.com>
2016-06-13 20:29:23 +0300

    Small UX improvement
    MultiUpload now hooks to MultiFileUpload (if in such) and triggers input from dropzone if clicked on it.

--- src/main/java/org/vaadin/easyuploads/client/ui/VMultiUpload.java
+++ src/main/java/org/vaadin/easyuploads/client/ui/VMultiUpload.java
@@ -7,6 +7,7 @@ import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Timer;
@@ -15,6 +16,7 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Hidden;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xhr.client.ReadyStateChangeHandler;
 import com.google.gwt.xhr.client.XMLHttpRequest;
 import com.vaadin.client.ApplicationConnection;
@@ -23,48 +25,52 @@ import com.vaadin.client.Paintable;
 import com.vaadin.client.UIDL;
 import com.vaadin.client.VConsole;
 import com.vaadin.client.ui.VButton;
+import com.vaadin.client.ui.VCssLayout;
+import com.vaadin.client.ui.VDragAndDropWrapper;
 import com.vaadin.client.ui.dd.VHtml5File;
 
 /**
  * Upload counterpart with "multiple" support.
- * 
+ *
  * Not finished enough for extension.
  */
 public class VMultiUpload extends SimplePanel implements Paintable {
 
-	private final class MyFileUpload extends FileUpload {
+    private HandlerRegistration clikcReg;
 
-		//  Starting from Chrome 30 we should prevent the onFocus hack or
-		//  otherwise the native file dialog is opened twice.
-		private final boolean isChrome30 = (BrowserInfo.get().isChrome() && BrowserInfo
-				.get().getBrowserMajorVersion() >= 30);
-	        
-		public MyFileUpload() {
-			getElement().setPropertyString("multiple", "multiple");
-		}
+    private final class MyFileUpload extends FileUpload {
 
-		@Override
-		public void onBrowserEvent(Event event) {
-			super.onBrowserEvent(event);
-			if (event.getTypeInt() == Event.ONCHANGE) {
-				if (hasFiles()) {
-					submit();
-				}
-			} else if (!isChrome30 && event.getTypeInt() == Event.ONFOCUS) {
-				// IE and user has clicked on hidden textarea part of upload
-				// field. Manually open file selector, other browsers do it by
-				// default.
-				fireNativeClick(fu.getElement());
-				// also remove focus to enable hack if user presses cancel
-				// button
-				fireNativeBlur(fu.getElement());
-			}
-		}
+        //  Starting from Chrome 30 we should prevent the onFocus hack or
+        //  otherwise the native file dialog is opened twice.
+        private final boolean isChrome30 = (BrowserInfo.get().isChrome() && BrowserInfo
+                .get().getBrowserMajorVersion() >= 30);
 
-		public boolean hasFiles() {
-			return getFileCount(getElement()) > 0;
-		}
-	}
+        public MyFileUpload() {
+            getElement().setPropertyString("multiple", "multiple");
+        }
+
+        @Override
+        public void onBrowserEvent(Event event) {
+            super.onBrowserEvent(event);
+            if (event.getTypeInt() == Event.ONCHANGE) {
+                if (hasFiles()) {
+                    submit();
+                }
+            } else if (!isChrome30 && event.getTypeInt() == Event.ONFOCUS) {
+                // IE and user has clicked on hidden textarea part of upload
+                // field. Manually open file selector, other browsers do it by
+                // default.
+                fireNativeClick(fu.getElement());
+                // also remove focus to enable hack if user presses cancel
+                // button
+                fireNativeBlur(fu.getElement());
+            }
+        }
+
+        public boolean hasFiles() {
+            return getFileCount(getElement()) > 0;
+        }
+    }
 
 	public static final native int getFileCount(Element el)
 	/*-{
@@ -78,123 +84,142 @@ public class VMultiUpload extends SimplePanel implements Paintable {
 	}-*/
 	;
 
-	public static final String CLASSNAME = "v-upload";
+    public static final String CLASSNAME = "v-upload";
 
-	private static final String DELIM = "---xXx---";
+    private static final String DELIM = "---xXx---";
 
-	/**
-	 * FileUpload component that opens native OS dialog to select file.
-	 */
-	MyFileUpload fu = new MyFileUpload();
+    /**
+     * FileUpload component that opens native OS dialog to select file.
+     */
+    MyFileUpload fu = new MyFileUpload();
 
-	Panel panel = new FlowPanel();
+    Panel panel = new FlowPanel();
 
-	ApplicationConnection client;
+    ApplicationConnection client;
 
-	private String paintableId;
+    private String paintableId;
 
-	/**
-	 * Button that initiates uploading
-	 */
-	private final VButton submitButton;
+    /**
+     * Button that initiates uploading
+     */
+    private final VButton submitButton;
 
-	private boolean enabled = true;
+    private boolean enabled = true;
 
-	private Hidden maxfilesize = new Hidden();
+    private Hidden maxfilesize = new Hidden();
 
-	private com.google.gwt.dom.client.Element synthesizedFrame;
+    private com.google.gwt.dom.client.Element synthesizedFrame;
 
-	private String receiverUri;
+    private String receiverUri;
 
-	private ReadyStateChangeHandler readyStateChangeHandler = new ReadyStateChangeHandler() {
-		public void onReadyStateChange(XMLHttpRequest xhr) {
-			if (xhr.getReadyState() == XMLHttpRequest.DONE) {
-				xhr.clearOnReadyStateChange();
-				VConsole.log("Ready state + " + xhr.getReadyState());
-				
-				Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-					public void execute() {
-						if (isAttached() && !fileQueue.isEmpty()) {
-							client.updateVariable(paintableId, "ready", true,
-									true);
-							postNextFileFromQueue();
-						}
-					}
-				});
-			}
-		}
+    private ReadyStateChangeHandler readyStateChangeHandler = new ReadyStateChangeHandler() {
+        public void onReadyStateChange(XMLHttpRequest xhr) {
+            if (xhr.getReadyState() == XMLHttpRequest.DONE) {
+                xhr.clearOnReadyStateChange();
+                VConsole.log("Ready state + " + xhr.getReadyState());
+
+                Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+                    public void execute() {
+                        if (isAttached() && !fileQueue.isEmpty()) {
+                            client.updateVariable(paintableId, "ready", true,
+                                    true);
+                            postNextFileFromQueue();
+                        }
+                    }
+                });
+            }
+        }
 	};;
 
 	public VMultiUpload() {
-		super(com.google.gwt.dom.client.Document.get().createDivElement());
+        super(com.google.gwt.dom.client.Document.get().createDivElement());
 
-		setWidget(panel);
-		panel.add(maxfilesize);
-		panel.add(fu);
-		submitButton = new VButton();
-		submitButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				// fire click on upload (eg. focused button and hit space)
-				fireNativeClick(fu.getElement());
-			}
-		});
-		panel.add(submitButton);
+        setWidget(panel);
+        panel.add(maxfilesize);
+        panel.add(fu);
+        submitButton = new VButton();
+        submitButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                // fire click on upload (eg. focused button and hit space)
+                fireNativeClick(fu.getElement());
+            }
+        });
+        panel.add(submitButton);
 
-		setStyleName(CLASSNAME);
-		fu.sinkEvents(Event.ONCHANGE);
-		fu.sinkEvents(Event.ONFOCUS);
-		addStyleName(CLASSNAME + "-immediate");
-	}
+        setStyleName(CLASSNAME);
+        fu.sinkEvents(Event.ONCHANGE);
+        fu.sinkEvents(Event.ONFOCUS);
+        addStyleName(CLASSNAME + "-immediate");
+    }
 
-	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
-		if (client.updateComponent(this, uidl, true)) {
-			return;
-		}
-		addStyleName(CLASSNAME + "-immediate");
+    public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+        if (client.updateComponent(this, uidl, true)) {
+            return;
+        }
+        addStyleName(CLASSNAME + "-immediate");
 
 
-		this.client = client;
-		paintableId = uidl.getId();
-		receiverUri = client.translateVaadinUri(uidl.getStringVariable("target"));
-		submitButton.setText(uidl.getStringAttribute("buttoncaption"));
-		fu.setName(paintableId + "_file");
+        this.client = client;
+        paintableId = uidl.getId();
+        receiverUri = client.translateVaadinUri(uidl.getStringVariable("target"));
+        submitButton.setText(uidl.getStringAttribute("buttoncaption"));
+        fu.setName(paintableId + "_file");
 
-		if (uidl.hasAttribute("disabled") || uidl.hasAttribute("readonly")) {
-			disableUpload();
-		} else if (!uidl.getBooleanAttribute("state")) {
-			// Enable the button only if an upload is not in progress
-			enableUpload();
-		}
-		if (uidl.hasAttribute("ready")) {
-			VConsole.log("The server knows about coming files. Start posting files");
-			postNextFileFromQueue();
-		}
-	}
+        if (uidl.hasAttribute("disabled") || uidl.hasAttribute("readonly")) {
+            disableUpload();
+        } else if (!uidl.getBooleanAttribute("state")) {
+            // Enable the button only if an upload is not in progress
+            enableUpload();
+        }
+        if (uidl.hasAttribute("ready")) {
+            VConsole.log("The server knows about coming files. Start posting files");
+            postNextFileFromQueue();
+        }
 
-	private void postNextFileFromQueue() {
-		if (!fileQueue.isEmpty()) {
-			final VHtml5File file = fileQueue.remove(0);
-			VConsole.log("Posting file " + file.getName() + " to "
-					+ receiverUri);
-			ExtendedXHR extendedXHR = (ExtendedXHR) ExtendedXHR.create();
-			extendedXHR.setOnReadyStateChange(readyStateChangeHandler);
-			extendedXHR.open("POST", receiverUri);
-			extendedXHR.postFile(file);
-			new Timer() {
-				@Override
-				public void run() {
-					// TODO poll for start or modify response so that we
-					// receive headers received
-					client.sendPendingVariableChanges();
-				}
-			}.schedule(700);
-		}
-	}
+        Widget parent = getParent();
+        if (clikcReg == null && parent instanceof VCssLayout) {
+            VCssLayout vCssLayout = (VCssLayout) parent;
+            if (vCssLayout.getStyleName().contains("v-multifileupload-uploads")) {
+                VCssLayout multifileupload = (VCssLayout) vCssLayout.getParent();
+                for (Widget widget : multifileupload) {
+                    if (widget.getStyleName().contains("v-multifileupload-dropzone")) {
+                        final VDragAndDropWrapper ddw = (VDragAndDropWrapper) widget;
+                        clikcReg = ddw.addDomHandler(new ClickHandler() {
+                            @Override
+                            public void onClick(ClickEvent event) {
+                                fireNativeClick(fu.getElement());
+                            }
+                        }, ClickEvent.getType());
+                    }
+                }
+            }
+        }
+    }
 
-	static class ExtendedXHR extends XMLHttpRequest {
+    private void postNextFileFromQueue() {
+        if (!fileQueue.isEmpty()) {
+            final VHtml5File file = fileQueue.remove(0);
+            VConsole.log("Posting file " + file.getName() + " to "
+                    + receiverUri);
+            ExtendedXHR extendedXHR = (ExtendedXHR) ExtendedXHR.create();
+            extendedXHR.setOnReadyStateChange(readyStateChangeHandler);
+            extendedXHR.open("POST", receiverUri);
+            extendedXHR.postFile(file);
+            new Timer() {
+                @Override
+                public void run() {
+                    // TODO poll for start or modify response so that we
+                    // receive headers received
+                    client.sendPendingVariableChanges();
+                }
+            }.schedule(700);
+        }
+    }
 
-		protected ExtendedXHR() {
-		}
+    static class ExtendedXHR extends XMLHttpRequest {
+
+        protected ExtendedXHR() {
+        }
 
 		public final native void postFile(VHtml5File file)
 		/*-{
@@ -203,7 +228,7 @@ public class VMultiUpload extends SimplePanel implements Paintable {
 		    this.send(file);
 		}-*/;
 
-	}
+    }
 
 	private static native void fireNativeClick(Element element)
 	/*-{
@@ -215,65 +240,78 @@ public class VMultiUpload extends SimplePanel implements Paintable {
 	    element.blur();
 	}-*/;
 
-	protected void disableUpload() {
-		submitButton.setEnabled(false);
-		// Cannot disable the fileupload while submitting or the file won't
-		// be submitted at all
-		fu.getElement().setPropertyBoolean("disabled", true);
-		enabled = false;
-	}
+    protected void disableUpload() {
+        submitButton.setEnabled(false);
+        // Cannot disable the fileupload while submitting or the file won't
+        // be submitted at all
+        fu.getElement().setPropertyBoolean("disabled", true);
+        enabled = false;
+    }
 
-	protected void enableUpload() {
-		submitButton.setEnabled(true);
-		fu.getElement().setPropertyBoolean("disabled", false);
-		enabled = true;
-	}
+    protected void enableUpload() {
+        submitButton.setEnabled(true);
+        fu.getElement().setPropertyBoolean("disabled", false);
+        enabled = true;
+    }
 
-	/**
-	 * Re-creates file input field and populates panel. This is needed as we
-	 * want to clear existing values from our current file input field.
-	 */
-	private void rebuildPanel() {
-		panel.remove(submitButton);
-		panel.remove(fu);
-		fu = new MyFileUpload();
-		fu.setName(paintableId + "_file");
-		fu.getElement().setPropertyBoolean("disabled", !enabled);
-		panel.add(fu);
-		panel.add(submitButton);
-		fu.sinkEvents(Event.ONCHANGE);
-	}
+    /**
+     * Re-creates file input field and populates panel. This is needed as we
+     * want to clear existing values from our current file input field.
+     */
+    private void rebuildPanel() {
+        panel.remove(submitButton);
+        panel.remove(fu);
+        fu = new MyFileUpload();
+        fu.setName(paintableId + "_file");
+        fu.getElement().setPropertyBoolean("disabled", !enabled);
+        panel.add(fu);
+        panel.add(submitButton);
+        fu.sinkEvents(Event.ONCHANGE);
+    }
 
-	private void submit() {
-		int files = getFileCount(fu.getElement());
-		String[] filedetails = new String[files];
-		for (int i = 0; i < files; i++) {
-			VHtml5File file = getFile(fu.getElement(), i);
-			queueFilePost(file);
-			filedetails[i] = serialize(file);
-		}
-		client.updateVariable(paintableId, "filequeue", filedetails, true);
+    private void submit() {
+        int files = getFileCount(fu.getElement());
+        String[] filedetails = new String[files];
+        for (int i = 0; i < files; i++) {
+            VHtml5File file = getFile(fu.getElement(), i);
+            queueFilePost(file);
+            filedetails[i] = serialize(file);
+        }
+        client.updateVariable(paintableId, "filequeue", filedetails, true);
 
-		disableUpload();
-	}
+        disableUpload();
+    }
 
-	/**
-	 * TODO should be part of VHtml5File
-	 * 
-	 * @param file
-	 * @return
-	 */
-	private String serialize(VHtml5File file) {
-		long size = (long) file.getSize();
-		String name = file.getName();
-		String type = file.getType();
-		return size + DELIM + name + DELIM + type + DELIM;
-	}
+    /**
+     * TODO should be part of VHtml5File
+     *
+     * @param file
+     * @return
+     */
+    private String serialize(VHtml5File file) {
+        long size = (long) file.getSize();
+        String name = file.getName();
+        String type = file.getType();
+        return size + DELIM + name + DELIM + type + DELIM;
+    }
 
-	private List<VHtml5File> fileQueue = new ArrayList<VHtml5File>();
+    private List<VHtml5File> fileQueue = new ArrayList<VHtml5File>();
 
-	private void queueFilePost(VHtml5File file) {
-		fileQueue.add(file);
-	}
+    private void queueFilePost(VHtml5File file) {
+        fileQueue.add(file);
+    }
+
+    @Override
+    protected void onDetach() {
+        super.onDetach();
+        deregisterFromMultiFileUploadDropArea();
+    }
+
+    private void deregisterFromMultiFileUploadDropArea() {
+        if (clikcReg != null) {
+            clikcReg.removeHandler();
+            clikcReg = null;
+        }
+    }
 
 }
