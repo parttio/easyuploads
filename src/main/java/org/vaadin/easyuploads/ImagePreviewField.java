3daeee1da51f6c28f03626dad448d74c9376ffd4
Matti Tahvonen <matti@vaadin.com>
2016-08-24 11:36:07 +0300

    Added ImagePreviewField for one of the most common use cases

---
+++ src/main/java/org/vaadin/easyuploads/ImagePreviewField.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016 Vaadin Community.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vaadin.easyuploads;
+
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.StreamResource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.themes.ValoTheme;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.imageio.ImageIO;
+
+/**
+ * A simple field to edit images stored as byte arrays (e.g. in database).
+ */
+public class ImagePreviewField extends UploadField {
+    
+    public ImagePreviewField() {
+        setAcceptFilter("image/*");
+        setFieldType(FieldType.BYTE_ARRAY);
+    }
+
+    @Override
+    protected Component createDisplayComponent() {
+        Image image = new Image();
+        image.setHeight("100px");
+        return image;
+    }
+
+    @Override
+    protected void updateDisplayComponent() {
+        try {
+            Image image = (Image) display;
+            // check if upload is an image
+            if (ImageIO.read(new ByteArrayInputStream((byte[]) getValue())) != null) {
+                // Update the image according to
+                // https://vaadin.com/book/vaadin7/-/page/components.embedded.html
+                SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+                String filename = df.format(new Date()) + getLastFileName();
+                StreamResource resource = new StreamResource(new ImageSource((byte[]) getValue()), filename);
+                resource.setCacheTime(0);
+                image.setSource(resource);
+            } else {
+                image.setSource(null);
+                setValue(null);
+            }
+            image.markAsDirty();
+            if (display.getParent() == null) {
+                getRootLayout().addComponent(display);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void attachDeleteButton(Button b) {
+        b.setIcon(FontAwesome.TRASH_O);
+        b.addStyleName(ValoTheme.BUTTON_ICON_ONLY);
+        b.addStyleName(ValoTheme.BUTTON_BORDERLESS);
+        getRootLayout().addComponent(b, 1);
+    }
+
+    @Override
+    protected String getDeleteCaption() {
+        return null;
+    }
+
+    public class ImageSource implements StreamResource.StreamSource {
+
+        private final byte[] buffer;
+
+        public ImageSource(byte[] image) {
+            this.buffer = image;
+        }
+
+        @Override
+        public InputStream getStream() {
+            return new ByteArrayInputStream(buffer);
+        }
+    }
+    
+}
---
+++ src/test/java/org/vaadin/easyuploads/demoandtestapp/CustomDisplayTestWithPojo.java
@@ -0,0 +1,111 @@
+package org.vaadin.easyuploads.demoandtestapp;
+
+import org.vaadin.easyuploads.ImagePreviewField;
+import com.vaadin.annotations.Theme;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+import javax.imageio.*;
+
+import org.vaadin.easyuploads.*;
+import org.vaadin.easyuploads.UploadField.FieldType;
+
+import com.vaadin.server.*;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
+import org.vaadin.addonhelpers.AbstractTest;
+import org.vaadin.viritin.BeanBinder;
+import org.vaadin.viritin.MBeanFieldGroup;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
+import org.vaadin.viritin.layouts.MVerticalLayout;
+
+@Theme("valo")
+public class CustomDisplayTestWithPojo extends AbstractTest {
+
+    private MBeanFieldGroup<Pojo> bfg;
+
+    public static class Pojo {
+
+        private String foo;
+        private byte[] photo;
+
+        public String getFoo() {
+            return foo;
+        }
+
+        public void setFoo(String foo) {
+            this.foo = foo;
+        }
+
+        public byte[] getPhoto() {
+            return photo;
+        }
+
+        public void setPhoto(byte[] photo) {
+            this.photo = photo;
+        }
+
+    }
+
+    Pojo pojo = new Pojo();
+
+    ImagePreviewField photo = new ImagePreviewField();
+
+    @Override
+    public Component getTestComponent() {
+        VerticalLayout layout = new MVerticalLayout();
+        photo.setCaption("Custom prview for images.");
+
+        Button button = new Button("Show photo state");
+        button.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                final byte[] photo1 = pojo.getPhoto();
+                if (photo1 == null) {
+                    Notification.show("EMPTY");
+                } else {
+                    String string = new String(photo1);
+                    if (string.length() > 100) {
+                        string = string.substring(0, 99);
+                    }
+                    Notification.show(string);
+                }
+            }
+        });
+
+        Button load = new Button("Load photo");
+        load.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                bfg.unbind();
+                InputStream resourceAsStream = getClass().getResourceAsStream("/boat.png");
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                try {
+                    IOUtils.copy(resourceAsStream, byteArrayOutputStream);
+                    pojo.setPhoto(byteArrayOutputStream.toByteArray());
+                    bfg = BeanBinder.bind(pojo, CustomDisplayTestWithPojo.this);
+                } catch (IOException ex) {
+                    Logger.getLogger(CustomDisplayTestWithPojo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        Button clear = new Button("clear value", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                pojo.setPhoto(null);
+                bfg = BeanBinder.bind(pojo, CustomDisplayTestWithPojo.this);
+            }
+        });
+
+        bfg = BeanBinder.bind(pojo, this);
+
+        layout.addComponents(photo, new MHorizontalLayout( button, load, clear));
+        return layout;
+    }
+
+}
