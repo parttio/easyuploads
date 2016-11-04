51ea3e8767c13a25ebe1086ef2edc7d38fd03211
Matti Tahvonen <matti@vaadin.com>
2016-11-04 18:22:05 +0200

    Added test for issue #21

---
+++ src/test/java/org/vaadin/easyuploads/demoandtestapp/ClearTest.java
@@ -0,0 +1,45 @@
+package org.vaadin.easyuploads.demoandtestapp;
+
+import com.vaadin.data.Property;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+
+import org.vaadin.easyuploads.*;
+
+import org.vaadin.addonhelpers.AbstractTest;
+
+public class ClearTest extends AbstractTest {
+
+    @Override
+    public Component getTestComponent() {
+        VerticalLayout layout = new VerticalLayout();
+        final UploadField uplDocument = new UploadField();
+        uplDocument.setButtonCaption("...Select File");
+        uplDocument.setDisplayUpload(false);
+        
+        uplDocument.addValueChangeListener(new Property.ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent event) {
+                Notification.show("File uploaded" + uplDocument.getLastFileName() + " IsEmpty:" + uplDocument.isEmpty());
+            }
+        });
+        
+        layout.addComponent(uplDocument);
+        
+        Button button = new Button("Test is clear + isEmtpy");
+        button.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                uplDocument.clear();
+                Notification.show("IsEmpty: " + uplDocument.isEmpty());
+            }
+        });
+        layout.addComponent(button);
+        
+        return layout;
+    }
+
+
+}
