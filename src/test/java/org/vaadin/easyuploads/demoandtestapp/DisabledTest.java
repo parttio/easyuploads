e8f8fec628bc739459fd39d6a1b6649de4099fbb
Matti Tahvonen <matti@vaadin.com>
2016-11-21 11:49:05 +0200

    Adding “raw Vaadin version” of the bug

--- src/test/java/org/vaadin/easyuploads/demoandtestapp/DisabledTest.java
+++ src/test/java/org/vaadin/easyuploads/demoandtestapp/DisabledTest.java
@@ -1,9 +1,13 @@
 package org.vaadin.easyuploads.demoandtestapp;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.data.Property;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.Upload;
 import com.vaadin.ui.VerticalLayout;
 import java.io.File;
 
@@ -11,6 +15,7 @@ import org.vaadin.easyuploads.*;
 
 import org.vaadin.addonhelpers.AbstractTest;
 
+@Theme("valo")
 public class DisabledTest extends AbstractTest {
 
     public static class MyBean {
@@ -52,17 +57,29 @@ public class DisabledTest extends AbstractTest {
 
         layout.addComponent(file);
         
+        
+        final Upload upload = new Upload();
+        upload.setCaption("Raw Vaadin upload");
+        
+        final Button b = new Button("Button");
+ 
+        final VerticalLayout wrap = new VerticalLayout(upload, b);
+        wrap.setEnabled(false);
+ 
         CheckBox cb = new CheckBox("Enabled");
         cb.setValue(false);
         cb.addValueChangeListener(new Property.ValueChangeListener() {
             @Override
             public void valueChange(Property.ValueChangeEvent event) {
-                final boolean name = !file.isEnabled();
-                file.setEnabled(name);
+                final boolean nextEnabled = !file.isEnabled();
+                file.setEnabled(nextEnabled);
+                wrap.setEnabled(nextEnabled);
+                boolean enabled = upload.isEnabled();
+                Notification.show("Enabled: " + enabled);
             }
         });
         
-        layout.addComponents(cb);
+        layout.addComponents(wrap, cb);
 
         return layout;
     }
